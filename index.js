2f2a3c5c42f8b2c8e41419e8420894713e1be2b5
XAIMOH001 <xaimohchau@gmail.com>
2026-01-19 01:31:39 +0300

    running external js file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('js linked successfully');
+ const apiKey = "a4600581b2e5a8bf442e6dae54b34466";
+  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+
+  async function checkWeather(){
+    const response = await fetch(apiUrl + '&appid=${apiKey}');
+    var data = await response.json();
+    console.log(data);
+  }
+checkWeather();
